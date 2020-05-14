9fd3f5ed2f00ef612aeb6f6e9711d9b7ae43eead
Sombrero13 <sombrero13nn@gmail.com>
2020-05-14 22:54:39 +0300

    Visual for collection finished

---
+++ src/Assets/RicoClient/Scripts/Menu/Modals/ModalWarn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Zenject;
+
+namespace RicoClient.Scripts.Menu.Modals
+{
+    public class ModalWarn : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _warnText = null;
+        [SerializeField]
+        private Button _okButton = null;
+        [SerializeField]
+        private Button _cancelButton = null;
+
+        public void SetWarnDialog(string text, Action okAction)
+        {
+            _warnText.text = text;
+            _okButton.onClick.AddListener(new UnityAction(okAction));
+
+            gameObject.SetActive(true);
+        }
+    }
+}
